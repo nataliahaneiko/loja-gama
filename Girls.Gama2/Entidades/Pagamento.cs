c5a24c44ab426e39c699bc355d62ff8a3a11b441
nataliaHaneiko <natata_brasil@yahoo.com.br>
2021-02-05 10:08:19 -0300

    Primeiro commit

---
+++ Girls.Gama2/Entidades/Pagamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girls.Gama2.Entidades
+{
+    public abstract class Pagamento : Produto
+    {
+        public Pagamento(double valor)
+        {
+            Id = Guid.NewGuid();
+            Valor = valor;
+            
+        }
+
+        public Guid Id { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public bool Confirmacao { get; set; }
+        public string Cpf { get; set; }
+
+
+        public virtual void Pagar()
+        {
+            DataPagamento = DateTime.Now;
+            Confirmacao = true;
+        }
+    }
+}
---
+++ Girls.Gama2/Entidades/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girls.Gama2.Entidades
+{
+    public class Produto
+    {
+     
+        public Produto(double valor,
+                       string nomeproduto)
+        {
+            NomeProduto = nomeproduto;
+            Valor = valor;
+        }
+
+        public string NomeProduto { get; set; }
+        public double Valor { get; set; }
+    }
+     
+
+
+}
+
+
+
